73b9192005ec787f57e2165e396a84dd48c1c0e5
lautarosartor <lautarosartor3@gmail.com>
2024-09-13 19:56:54 -0300

    Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./views/public/Home"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Services from "./views/public/Services"
+import About from "./views/public/About"
+import Testimonials from "./views/public/Testimonials"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/"
+          element={
+            <>
+              <Home />
+              <Services />
+              <About />
+              <Testimonials />
+            </>
+          }
+        />
+        <Route path="*" element={<p>Not Found...</p>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
