9fac2d18c3d7c1941bb806963a320d42984ad266
lautarosartor <lautarosartor3@gmail.com>
2024-09-26 20:40:38 -0300

    feat: Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,97 @@
+import { Button, Collapse, useDisclosure } from '@chakra-ui/react'
+import logo from '../assets/logo.png'
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
+import './components.css'
+
 function Header() {
+  const { isOpen, onToggle } = useDisclosure()
+
   return (
-    <div>Header</div>
+    <nav className="bg-[#3B341F] text-[#EFFFC8]">
+      <div className="md:flex container mx-auto p-6 items-center justify-between hidden">
+        <img src={logo} className="w-16" alt="logo" />
+        <ul className="flex gap-6">
+          <li>
+            <a href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#menu">
+              Menu
+            </a>
+          </li>
+          <li>
+            <a href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="md:hidden p-6">
+        <div className="flex justify-between">
+          <img src={logo} className="w-16" alt="logo" />
+          <Button onClick={onToggle}>
+            {!isOpen
+              ? <HamburgerIcon className="text-3xl" />
+              : <CloseIcon className="text-1xl" />
+            }
+          </Button>
+        </div>
+
+        <Collapse in={isOpen} animateOpacity unmountOnExit>
+          <ul className="bg-[#3B341Fee] text-[#EFFFC8] absolute left-0 p-6 w-full flex flex-col items-end gap-6 z-10" style={{backdropFilter: 'blur(4px)'}}>
+            <li>
+              <a href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#menu">
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#services">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </Collapse>
+      </div>
+      
+    </nav>
   )
 }
 
