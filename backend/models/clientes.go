004dc60046005f5a86959ce74aee49ce2a281ea0
lautarosartor <lautarosartor3@gmail.com>
2024-10-02 17:22:33 -0300

    fix: models modificated

--- backend/models/clientes.go
+++ backend/models/clientes.go
@@ -4,8 +4,10 @@ type Clientes struct {
 	ID       uint      `json:"id" gorm:"primary_key"`
 	Idsesion uint      `json:"idsesion"`
 	Idmesa   uint      `json:"idmesa"`
+	Idrol    uint      `json:"idrol"`
 	Sesion   *Sesiones `json:"sesion,omitempty" gorm:"ForeignKey:idsesion;AssociationForeignKey:id"`
 	Mesa     *Mesas    `json:"mesa,omitempty" gorm:"ForeignKey:idmesa;AssociationForeignKey:id"`
+	Rol      *Roles    `json:"rol,omitempty" gorm:"ForeignKey:idrol;AssociationForeignKey:id"`
 }
 
 func (Clientes) TableName() string {
---
+++ backend/models/usuarios.go
@@ -0,0 +1,29 @@
+package models
+
+import "time"
+
+type Usuarios struct {
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Idrol     uint      `json:"idrol"`
+	Nombre    string    `json:"nombre"`
+	Apellido  string    `json:"apellido"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Telefono  *string   `json:"telefono"`
+	Estado    bool      `json:"estado"`
+	CreatedAt time.Time `json:"created_at"`
+	Rol				*Roles		`json:"rol,omitempty" gorm:"ForeignKey:idrol;AssociationForeignKey:id"`
+}
+
+func (Usuarios) TableName() string {
+	return "usuarios"
+}
+
+type Roles struct {
+	ID     uint   `json:"id" gorm:"primary_key"`
+	Nombre string `json:"nombre"`
+}
+
+func (Roles) TableName() string {
+	return "roles"
+}
