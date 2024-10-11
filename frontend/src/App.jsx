c2de74d74e22b3cf96654ed67ba4f080c600970c
lautarosartor <lautarosartor3@gmail.com>
2024-10-11 19:16:57 -0300

    feat: definition of some private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,31 +9,64 @@ import NotFound from "./components/NotFound"
 import Menu from "./views/public/Menu"
 import { ChakraProvider } from "@chakra-ui/react"
 import Contact from "./views/public/Contact"
+import PrivateRoute from "./providers/PrivateRoute"
+import Productos from "./views/private/productos/Productos"
+import Login from "./views/private/auth/Login"
+import Register from "./views/private/auth/Register"
+import Dashboard from "./views/private/Dashboard"
+import Mesas from "./views/private/mesas/Mesas"
+import Sesiones from "./views/private/sesiones/Sesiones"
 
 function App() {
 
   return (
     <ChakraProvider>
       <BrowserRouter>
-        <Header />
-        <main className="flex-grow container mx-auto my-20 space-y-20 md:my-32 md:space-y-32 px-6 md:px-10 xl:px-28">
-          <Routes>
-            <Route exact path="/"
-              element={
-                <>
-                  <Home />
-                  <About />
-                  <Menu />
-                  <Services />
-                  <Testimonials />
-                  <Contact />
-                </>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
+        <Routes>
+          {/* Rutas públicas */}
+          <Route path="/*" element={
+            <>
+              <Header />
+              <main className="flex-grow container mx-auto my-20 space-y-20 md:my-32 md:space-y-32 px-6 md:px-10 xl:px-28">
+                <Routes>
+                  <Route exact path="/"
+                    element={
+                      <>
+                        <Home />
+                        <About />
+                        <Menu />
+                        <Services />
+                        <Testimonials />
+                        <Contact />
+                      </>
+                    }
+                  />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </>
+          } />
+
+          {/* Autenticacion */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Rutas privadas */}
+          <Route path="/admin/*"
+            element={
+              <PrivateRoute> {/* Protege las rutas de administración */}
+                <Routes>
+                  <Route path="dashboard" element={<Dashboard />} />
+                  <Route path="mesas" element={<Mesas />} />
+                  <Route path="productos" element={<Productos />} />
+                  <Route path="sesiones" element={<Sesiones />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </PrivateRoute>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </ChakraProvider>
   )
