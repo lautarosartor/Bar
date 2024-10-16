287cd05b920816e39429c42b51327f457bae1345
lautarosartor <lautarosartor3@gmail.com>
2024-10-16 19:36:24 -0300

    fix on private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import About from "./views/public/About"
 import Testimonials from "./views/public/Testimonials"
 import NotFound from "./components/NotFound"
 import Menu from "./views/public/Menu"
-import { ChakraProvider } from "@chakra-ui/react"
 import Contact from "./views/public/Contact"
 import PrivateRoute from "./providers/PrivateRoute"
 import Productos from "./views/private/productos/Productos"
@@ -16,59 +15,59 @@ import Register from "./views/private/auth/Register"
 import Dashboard from "./views/private/Dashboard"
 import Mesas from "./views/private/mesas/Mesas"
 import Sesiones from "./views/private/sesiones/Sesiones"
+import Usuarios from "./views/private/usuarios/Usuarios"
 
 function App() {
 
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Rutas públicas */}
-          <Route path="/*" element={
-            <>
-              <Header />
-              <main className="flex-grow container mx-auto my-20 space-y-20 md:my-32 md:space-y-32 px-6 md:px-10 xl:px-28">
-                <Routes>
-                  <Route exact path="/"
-                    element={
-                      <>
-                        <Home />
-                        <About />
-                        <Menu />
-                        <Services />
-                        <Testimonials />
-                        <Contact />
-                      </>
-                    }
-                  />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </main>
-              <Footer />
-            </>
-          } />
+    <BrowserRouter>
+      <Routes>
+        {/* Rutas públicas */}
+        <Route path="/*" element={
+          <>
+            <Header />
+            <main className="flex-grow container mx-auto my-20 space-y-20 md:my-32 md:space-y-32 px-6 md:px-10 xl:px-28">
+              <Routes>
+                <Route exact path="/"
+                  element={
+                    <>
+                      <Home />
+                      <About />
+                      <Menu />
+                      <Services />
+                      <Testimonials />
+                      <Contact />
+                    </>
+                  }
+                />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </>
+        } />
 
-          {/* Autenticacion */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+        {/* Autenticacion */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-          {/* Rutas privadas */}
-          <Route path="/admin/*"
-            element={
-              <PrivateRoute> {/* Protege las rutas de administración */}
-                <Routes>
-                  <Route path="dashboard" element={<Dashboard />} />
-                  <Route path="mesas" element={<Mesas />} />
-                  <Route path="productos" element={<Productos />} />
-                  <Route path="sesiones" element={<Sesiones />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
+        {/* Rutas privadas */}
+        <Route path="/admin/*"
+          element={
+            <PrivateRoute> {/* Protege las rutas de administración */}
+              <Routes>
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="mesas" element={<Mesas />} />
+                <Route path="productos" element={<Productos />} />
+                <Route path="sesiones" element={<Sesiones />} />
+                <Route path="usuarios" element={<Usuarios />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/providers/PrivateRoute.jsx
+++ frontend/src/providers/PrivateRoute.jsx
@@ -1,10 +1,12 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Grid, GridItem } from '@chakra-ui/react'
 
 const PrivateRoute = ({ children }) => {
   // Aquí es donde comprobarías si el usuario está autenticado
   const isAuthenticated = sessionStorage.getItem("token");
+
+  const navigate = useNavigate();
 
   // Si esta authenticado muestra los componentes, sino lo redirige a logearse
   if (isAuthenticated) {
@@ -15,41 +17,59 @@ const PrivateRoute = ({ children }) => {
           templateRows='repeat(1, 1fr)'
           templateColumns='repeat(6, 1fr)'
         >
-          <GridItem rowSpan={2} colSpan={1} bg="#3B341F" p={4}>
-            <nav className="text-xl text-[#EFFFC8]">
-              <a className="flex justify-center font-black text-2xl mb-10 text-[#85CB33]" href="/">
-                Rest In Beer
-              </a>
+          {/*NAV*/}
+          <GridItem as="nav" rowSpan={2} colSpan={1} bg="#3B341F" textColor="#EFFFC8" p={4}>
+            <a className="flex justify-center font-black text-2xl mb-10 text-[#85CB33]" href="/">
+              Rest In Beer
+            </a>
 
-              <ul className="space-y-6">
-                <li>
-                  <a href="/admin/dashboard">
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a href="/admin/mesas" disabled>
-                    Mesas
-                  </a>
-                </li>
-                <li>
-                  <a href="/admin/productos">
-                    Productos
-                  </a>
-                </li>
-                <li>
-                  <a href="/admin/sesiones" disabled>
-                    Sesiones
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <ul className="space-y-6">
+              <li>
+                <a
+                  className={location.pathname === "/admin/dashboard" ? "active-link" : ""}
+                  onClick={() => navigate("/admin/dashboard")}
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <a
+                  className={location.pathname === "/admin/mesas" ? "active-link" : ""}
+                  onClick={() => navigate("/admin/mesas")}
+                >
+                  Mesas
+                </a>
+              </li>
+              <li>
+                <a
+                  className={location.pathname === "/admin/productos" ? "active-link" : ""}
+                  onClick={() => navigate("/admin/productos")}
+                >
+                  Productos
+                </a>
+              </li>
+              <li>
+                <a
+                  className={location.pathname === "/admin/sesiones" ? "active-link" : ""}
+                  onClick={() => navigate("/admin/sesiones")}
+                >
+                  Sesiones
+                </a>
+              </li>
+              <li>
+                <a
+                  className={location.pathname === "/admin/usuarios" ? "active-link" : ""}
+                  onClick={() => navigate("/admin/usuarios")}
+                >
+                  Usuarios
+                </a>
+              </li>
+            </ul>
           </GridItem>
           
-          <GridItem colSpan={5} p={4}>
-            <main className="py-10">
-              {children}
-            </main>
+          {/*MAIN*/}
+          <GridItem as="main" colSpan={5} p={4}>
+            {children}
           </GridItem>
         </Grid>
       </div>
